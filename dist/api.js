ebb6af8d21b0ebc7d3693f381638d6a82fbe6f9f
Joonas Yliaho <joonas.yliaho@gmail.com>
2019-02-25 12:20:45 +0200

    remove dist

--- dist/api.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var map_router_1 = require("./map/map.router");
-exports.router = express_1.Router();
-// Map
-exports.router.use('/map', map_router_1.router);
--- dist/map/map.route.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var getShores_1 = require("./controllers/getShores");
-exports.router = express_1.Router();
-exports.router.get('/shores', getShores_1.getShores);
