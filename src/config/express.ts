a3a389ed74ca3a9c11adafdc426b0c45d39b3b5c
obb12 <omar.bb@hotmail.fi>
2019-03-08 16:10:50 +0200

    added data to the backend

--- src/config/express.ts
+++ src/config/express.ts
@@ -1,10 +1,11 @@
 import express from 'express'
 import bodyparser from 'body-parser'
 import { router as apiRouter } from '@/api'
-
+import cors from 'cors'
 export const app = express()
-
+//cors
+app.use(cors())
 // Bodyparser
 app.use(bodyparser.urlencoded({ extended: false }))
-
+  app.use(express.static('public'));
 app.use('/api', apiRouter)
