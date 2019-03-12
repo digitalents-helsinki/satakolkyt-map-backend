8df7c234da3b7c6ed5040332431dc3f81a1393d3
Joonas Yliaho <joonas.yliaho@gmail.com>
2019-03-12 16:28:26 +0200

    add /shore/:key endpoint

---
+++ src/map/controllers/getShore.ts
@@ -0,0 +1,19 @@
+import { aql } from 'arangojs'
+import { db } from '@/config/arangodb'
+import { RequestHandler } from 'express'
+
+/**
+ * Returns all geosjon feature objects from the db collection.
+ */
+export const getShore: RequestHandler = async (req, res, next) => {
+  try {
+    console.log(req.params.key)
+    const geojsonFeaturesCollection = db.collection('geojson_features')
+
+    const document = await geojsonFeaturesCollection.document(req.params.key)
+
+    res.send({ data: document })
+  } catch (err) {
+    res.send({ error: err.message })
+  }
+}
--- src/map/map.router.ts
+++ src/map/map.router.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express'
 import { getShores } from './controllers/getShores'
+import { getShore } from './controllers/getShore'
 
 export const router = Router()
 
 router.get('/shores', getShores)
+router.get('/shore/:key', getShore)
