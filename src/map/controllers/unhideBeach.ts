a5eb1c4c33a5e73c68023d5def7253903d2c3b83
Samu Suvanto <samu.suvanto@gmail.com>
2019-04-24 11:01:58 +0300

    send back shore info when unhiding shore

--- src/map/controllers/unhideBeach.ts
+++ src/map/controllers/unhideBeach.ts
@@ -4,12 +4,10 @@ import { RequestHandler } from 'express'
 export const unhideBeach: RequestHandler = async (req, res, next) => {
   try {
     const { _key } = await ShoreModel.updateShoreDocument(req.body.key, {
-        state: { status: 'free', data: 'x', foo: 'bar' }
+      state: { status: 'free', data: 'x', foo: 'bar' }
     })
 
-
-
-    res.send({status: "ok"})
+    res.send({ json: await ShoreModel.getShore(_key), status: 'ok' })
     res.end()
   } catch (err) {
     res.send({ error: err.message })
