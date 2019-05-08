2b632427f6ffac1d45263546479c2b09b4aead3c
Samu Suvanto <samu.suvanto@gmail.com>
2019-05-08 14:04:04 +0300

    check if shore already reserved. other reservation error handling.

--- src/map/controllers/reserveBeach.ts
+++ src/map/controllers/reserveBeach.ts
@@ -11,26 +11,42 @@ const collection = db.collection('reservations')
 export const reserveBeach: RequestHandler = async (req, res, next) => {
   try {
     const data = req.body
+
+    //check if this piece of shore is actually available
+    const shore = await ShoreModel.getShore(data.selected.key)
+    if (shore.status && shore.status !== 'free') {
+      //409: Conflict (should this be used?)
+      console.log('Shore unavailable')
+      return res.status(409).send({ error: 'err_shoreunavailable' })
+    }
+
+    //add the requesting user's ip to the data
     data.userip = req.connection.remoteAddress
+
     console.log(data)
+
+    //check data format validation errors
+    const errors = validationResult(req)
+    if (!errors.isEmpty() && false) {
+      //disable validation during dev
+      //422: Unprocessable Entity
+      return res.status(422).send({ error: 'err_validationerror' })
+    }
+
+    //everything ok, save reservation to db
     collection
       .save(data)
       .then(
         meta => console.log('Document saved:', meta._rev),
         err => console.error('Failed to save document:', err)
       )
-    const validationOn = false // disable validation during dev
-    const errors = validationResult(req)
-    if (!errors.isEmpty() && validationOn) {
-      return res.status(422).json({ errors: errors.array() })
-    }
-    //everything ok, set shore as reserved
+    //and set the shore status to reserved
     const { _key } = await ShoreModel.updateShoreDocument(
       req.body.selected.key,
       { status: 'reserved' }
     )
 
-    res.send({ json: await ShoreModel.getShore(_key), status: 'ok' })
+    res.send({ json: shore, status: 'ok' })
     res.end()
   } catch (err) {
     res.send({ error: err.message })
