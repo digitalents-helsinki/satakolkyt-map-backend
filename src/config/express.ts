8655947442b6e44fa74d6e8111035755325cada9
Vesa Meriläinen <vesamatti.merilainen93@gmail.com>
2019-05-10 12:05:16 +0300

    fix header permissions

--- src/config/express.ts
+++ src/config/express.ts
@@ -9,9 +9,9 @@ app.use(cookieParser())
 
 //cors
 var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8080')
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  res.header('Access-Control-Allow-Headers', 'Content-Type,x-csrf-token')
   res.header('Access-Control-Allow-Credentials', 'true')
 
   next()
