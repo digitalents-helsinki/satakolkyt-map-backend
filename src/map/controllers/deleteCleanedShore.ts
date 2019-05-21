a977410dd8f6b2938bd6e33809f4f0314d5877fd
Samu Suvanto <samu.suvanto@gmail.com>
2019-05-21 10:37:03 +0300

    when deleting cleaninfo, if confirmed, take away from steps/km

--- src/map/controllers/deleteCleanedShore.ts
+++ src/map/controllers/deleteCleanedShore.ts
@@ -1,5 +1,6 @@
 import ShoreModel from '../model/shore'
 import CleanInfoModel from '../model/cleaninfo'
+import StepsKmModel from '../model/steps_km_info'
 import { RequestHandler } from 'express'
 
 /**
@@ -8,9 +9,21 @@ import { RequestHandler } from 'express'
 export const deleteCleanedShore: RequestHandler = async (req, res, next) => {
   try {
     const shore = await ShoreModel.getShore(req.body.key)
+
     const { _key } = await ShoreModel.updateShoreDocument(req.body.key, {
       status: shore.hasReservation ? 'reserved' : 'free'
     })
+
+    const cleaninfo = await CleanInfoModel.getCleanedByKey(req.body.id)
+    if (cleaninfo.confirmed) {
+      const stepskm = await StepsKmModel.getStepsKmInfo()
+      const newsteps =
+        stepskm.steps -
+        Math.floor(cleaninfo.group_size * (shore.properties.length / 0.6))
+      const newkm = stepskm.km - shore.properties.length / 1000
+      StepsKmModel.updateStepsKmInfo(newsteps, newkm)
+    }
+
     console.log('Removing cleaned shore ' + req.body.id)
     await CleanInfoModel.removeCleaned(req.body.id)
 
