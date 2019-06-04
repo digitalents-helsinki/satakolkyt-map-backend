e60fef06240dbcd41947f5f1a99bec6e037e23f8
Samu Suvanto <samu.suvanto@gmail.com>
2019-06-04 13:28:18 +0300

    add basic version of cleaning confirmation email

--- src/map/controllers/confirmCleaned.ts
+++ src/map/controllers/confirmCleaned.ts
@@ -4,9 +4,11 @@ import ShoreModel from '../model/shore'
 import CleanInfoModel from '../model/cleaninfo'
 import StepsKmModel from '../model/steps_km_info'
 import { RequestHandler } from 'express'
+import { sendMail } from '../../mail'
 
 export const confirmCleaned: RequestHandler = async (req, res, next) => {
   try {
+    const cleaninfo = await CleanInfoModel.getCleanedByKey(req.body.clean)
     const { clean } = await CleanInfoModel.updateCleaned(req.body.clean, {
       confirmed: true,
       leader_name: '---',
@@ -14,7 +16,6 @@ export const confirmCleaned: RequestHandler = async (req, res, next) => {
       leader_phone: '---'
     })
 
-    const cleaninfo = await CleanInfoModel.getCleanedByKey(req.body.clean)
     const shoreinfo = await ShoreModel.getShore(req.body.key)
     const stepskm = await StepsKmModel.getStepsKmInfo()
     const newsteps =
@@ -25,6 +26,12 @@ export const confirmCleaned: RequestHandler = async (req, res, next) => {
 
     res.send({ status: 'ok' })
     res.end()
+
+    sendMail(
+      cleaninfo.leader_email,
+      'Siivouksenne on vahvistettu',
+      'Siivouksenne on nyt vahvistettu. SATAKOLKYT kiittää! Henkilötietonne on poistettu järjestelmästä.'
+    )
   } catch (err) {
     res.send({ error: err.message })
   }
