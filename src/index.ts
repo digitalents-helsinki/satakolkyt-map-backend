e3603c213f31207f1aa522f99f85fd1eadaa832a
Vesa Meriläinen <vesamatti.merilainen93@gmail.com>
2019-06-13 11:53:10 +0300

    remove sentry

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,10 @@ import './env'
 import { connect } from './config/arangodb'
 import { config } from './config/index'
 import { app } from './config/express'
-const Sentry = require('@sentry/node')
 
 const start = async () => {
   try {
     await connect()
-    Sentry.init({
-      dsn: 'https://04bfcb6dc6534683894daf06d484339f@sentry.io/1446248'
-    })
 
     app.listen(config.port, () => {
       console.log('App listening on port ' + config.port)
