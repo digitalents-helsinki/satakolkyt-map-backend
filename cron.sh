a3edb3fe77830ac133ee5e5e2c8ef7a2416b0fc4
Samu Suvanto <samu.suvanto@gmail.com>
2019-06-13 13:09:14 +0300

    cron setup file, password in env file

---
+++ cron.sh
@@ -0,0 +1,5 @@
+CRON_EMAIL_PASS=$(grep CRON_EMAIL_PASS .env | xargs)
+IFS='=' read -ra CRON_EMAIL_PASS <<< "$CRON_EMAIL_PASS"
+CRON_EMAIL_PASS=${CRON_EMAIL_PASS[1]}
+
+(crontab -l ; echo "0 3 * * * curl localhost:8089/api/map/cleanupemail/$CRON_EMAIL_PASS") | sort - | uniq - | crontab -
--- src/map/controllers/sendCleanupEmail.ts
+++ src/map/controllers/sendCleanupEmail.ts
@@ -1,15 +1,15 @@
 import { RequestHandler } from 'express'
 
-//RUN THIS FILE WITH CRON, ADD TO CRONTAB:
-// 0 3 * * * curl localhost:8089/api/map/cleanupemail/[passhere]
+//THIS IS RUN BY CRON AFTER cron.sh HAS BEEN EXECUTED
 
 export const sendCleanupEmail: RequestHandler = async (req, res, next) => {
   const pass = req.params.pw
-  console.log('Cron email test. Pass:', pass)
-  if (pass === 'test123') {
-    //TODO: get from env file
+  console.log('Running cleanup email send routine...')
+  if (pass === process.env.CRON_EMAIL_PASS) {
     console.log('Pass OK')
+
     //TODO: Implement mail logic
+
     res.sendStatus(200)
   } else {
     res.sendStatus(403)
