b14aaa6a8f835adeeb81a4e193c26e653ea7b1d8
Topias Rytkönen <topias.rytkonen@digitalentshelsinki.fi>
2020-02-04 12:06:32 +0200

    Change cron timing

--- cron.sh
+++ cron.sh
@@ -2,4 +2,4 @@ CRON_EMAIL_PASS=$(grep CRON_EMAIL_PASS .env | xargs)
 IFS='=' read -ra CRON_EMAIL_PASS <<< "$CRON_EMAIL_PASS"
 CRON_EMAIL_PASS=${CRON_EMAIL_PASS[1]}
 
-(crontab -l ; echo "0 3 * * * curl localhost:8089/api/map/cleanupemail/$CRON_EMAIL_PASS") | sort - | uniq - | crontab -
+(crontab -l ; echo "0 7 * * * curl localhost:8089/api/map/cleanupemail/$CRON_EMAIL_PASS") | sort - | uniq - | crontab -
