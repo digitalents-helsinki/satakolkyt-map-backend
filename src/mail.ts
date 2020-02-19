9df7a7626ee2653fa10e59f2e054fb7a01f101b5
Topias Rytkönen <topias.rytkonen@digitalentshelsinki.fi>
2020-02-19 13:21:29 +0200

    Use path join for attachments

--- src/mail.ts
+++ src/mail.ts
@@ -1,4 +1,5 @@
 const nodemailer = require('nodemailer')
+import { join } from 'path'
 
 const transporter = nodemailer.createTransport({
   host: 'mail04.domainhotelli.fi',
@@ -26,22 +27,22 @@ export const sendMail = (to, subject, html, options: sendMailOptions = {}) => {
       attachments: [
         {
           filename: 'fbook.png',
-          path: process.cwd() + '/assets/fbook.png',
+          path: join(process.cwd(), '/assets/fbook.png'),
           cid: 'd0557c41-f196-41fd-abbf-cfe3dd5356dd'
         },
         {
           filename: 'inst.png',
-          path: process.cwd() + '/assets/inst.png',
+          path: join(process.cwd(), '/assets/inst.png'),
           cid: '350673a5-5d18-4966-a9cb-fde89287b726'
         },
         {
           filename: 'twit.png',
-          path: process.cwd() + '/assets/twit.png',
+          path: join(process.cwd(), '/assets/twit.png'),
           cid: 'abb0ad98-b191-4afb-8266-4b97c05938ea'
         },
         {
           filename: 'satakolkyt.jpeg',
-          path: process.cwd() + '/assets/satakolkyt.jpeg',
+          path: join(process.cwd(), '/assets/satakolkyt.jpeg'),
           cid: '29137d21-03d2-4f7f-a944-4c0949dff9e4'
         }
       ]
